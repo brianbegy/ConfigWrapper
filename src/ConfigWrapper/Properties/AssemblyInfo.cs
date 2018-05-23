cf8f9ec4218a2dae9c6bf26a764d0c693c54c403
Brian Begy <bbegy@grubhub.com>
2018-05-23 11:20:27 -0500

    nuget stuff

--- src/ConfigWrapper/Properties/AssemblyInfo.cs
+++ src/ConfigWrapper/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ConfigWrapper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple configuration library for working with app.config and web.config.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Brian Begy")]
 [assembly: AssemblyProduct("ConfigWrapper")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Brian Begy 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
---
+++ src/nuget-build.ps1
@@ -0,0 +1,1 @@
+nuget pack .\ConfigWrapper\ConfigWrapper.nuspec
