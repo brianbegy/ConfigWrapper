61293d22b7e42b0e52f6169f5997b00a9c0f4850
Brian Begy <bbegy@grubhub.com>
2018-06-12 09:24:54 -0500

    fix bad build.

--- src/nuget-build.ps1
+++ src/nuget-build.ps1
@@ -6,11 +6,12 @@ get-childItem ConfigWrapper.Json/packages -recurse | remove-item -recurse -force
 ## restore all nuget packages
 nuget restore ConfigWrapper.sln
 
-## clean and build the soluion
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe ConfigWrapper.sln  /t:Clean
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe ConfigWrapper.sln  /t:build /p:Configuration=Release /p:TargetFramework=v4.0
+rm .\ConfigWrapper.*.*.*.nupkg
 
- rm .\ConfigWrapper.*.*.*.nupkg
+## clean and build the soluion
+msbuild ConfigWrapper.sln  /t:Clean
+msbuild ConfigWrapper/ConfigWrapper.csproj  /p:Configuration=Release /p:TargetFramework=v4.0 
+msbuild ConfigWrapper/ConfigWrapperJson.csproj  /p:Configuration=Release /p:TargetFramework=v4.0 
  
- nuget pack .\ConfigWrapper\ConfigWrapper.nuspec
- nuget pack .\ConfigWrapper.Json\ConfigWrapper.json.nuspec
+nuget pack .\ConfigWrapper\ConfigWrapper.nuspec
+nuget pack .\ConfigWrapper.Json\ConfigWrapper.json.nuspec
